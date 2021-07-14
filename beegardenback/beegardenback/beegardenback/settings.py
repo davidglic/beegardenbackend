dffe46040b7bf4a3d86c093e033eb6d3e43fc575
David Lewis <david_lewis@glic.com>
2021-07-14 15:52:24 -0700

    cors error: edited path

--- beegardenback/beegardenback/beegardenback/settings.py
+++ beegardenback/beegardenback/beegardenback/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://rebel-friends.surge.sh/'
+    'https://rebel-friends.surge.sh'
 ]
 
 JWT_TOKENS = os.getenv('JWT_TOKENS', 'adummykeyhere115')
